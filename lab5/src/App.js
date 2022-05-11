2dd0b422622634e13d54307ab0fb6a69f44c5b17
Huang Yan <huangyan@Huangs-MacBook-Air.local>
2022-05-11 18:05:39 -0400

    done with lab5

--- lab5/src/App.js
+++ lab5/src/App.js
@@ -3,14 +3,15 @@ import React from "react"
 export default class App extends React.Component{
   state = {
     loading: true,
-    cars: null,
+    car: null,
+    car2: null,
   };
   async componentDidMount(){
     const url = "http://localhost:8000/cars#"
     const response = await fetch(url);
     const data = await response.json();
     this.setState({car: data.cars[0],loading:false})
-    
+    this.setState({car2: data.cars[1],loading:false})
     
   };
 
@@ -27,7 +28,35 @@ export default class App extends React.Component{
     return (
       
       <div>
-        <div>{this.state.car.fname}</div>
+        <table border = "1">
+          <tr>
+            <td>Car id</td>
+            <td>first name</td>
+            <td>last name</td>
+            <td>make</td>
+            <td>model</td>
+            <td>year</td>
+            <td>score</td>
+          </tr>
+          <tr>
+            <td>{this.state.car.carid}</td>
+            <td>{this.state.car.fname}</td>
+            <td>{this.state.car.lname}</td>
+            <td>{this.state.car.make}</td>
+            <td>{this.state.car.model}</td>
+            <td>{this.state.car.year}</td>
+            <td>{this.state.car.score}</td>
+          </tr>
+          <tr>
+          <td>{this.state.car2.carid}</td>
+            <td>{this.state.car2.fname}</td>
+            <td>{this.state.car2.lname}</td>
+            <td>{this.state.car2.make}</td>
+            <td>{this.state.car2.model}</td>
+            <td>{this.state.car2.year}</td>
+            <td>{this.state.car2.score}</td>
+          </tr>
+        </table>
       </div>
     )
   }
